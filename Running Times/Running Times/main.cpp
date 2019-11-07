931cb9ab6d48b4cf1add0958bcf00287f0b8154b
Angelo Pablo <angelopablo014@gmail.com>
2019-11-07 16:14:33 -0700

    started running times application

---
+++ Running Times/Running Times/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Running Times
+//
+//  Created by Angelo Pablo on 11/7/19.
+//  Copyright © 2019 Angelo Pablo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
